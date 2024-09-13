bacc245d8498b7dca1d958537ff86fb1c0268a1e
Alexandr-K1 <alexsa1995@gmail.com>
2024-09-13 22:56:02 +0300

    first_commit

---
+++ HW_M4_T1.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+def total_salary(path):
+    salary = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                salary.append(line.split(",")[1])
+            total = sum(int(salary))
+            avarage = total / len(salary)
+    except FileNotFoundError:
+        return "File not found Error!" 
+    except OSError:
+        return "Error while reading file!"
+    except ValueError:
+        return "Error: File does not contain integers!"  
+    return total, avarage  
+
+path = Path('path/to/salary_file.txt') 
+total, avarage = total_salary(path)
+print(f'Загальна сума заробітної плати: {total}, Середня заробітна плата: {avarage}')
